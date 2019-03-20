b680a45c5707c940799199efddc6585b1e1709fe
Naelle Zephir <nzephir@bu.edu>
2019-03-20 16:59:59 -0400

    updated models

--- foodshare/food/models.py
+++ foodshare/food/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+	name =models.CharField(max_length=265)
 	is_restaurant = models.BooleanField('restaurant status', default=False)
 	is_person = models.BooleanField('person status', default=False)
 	
 class Restaurant(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-	name = models.CharField(max_length=265)
 	amount_gold = models.IntegerField()
 	food_available = models.BooleanField(default = False)
 	phone = models.CharField(max_length=265)
