2bcd3469b0ddfc3d3d49d17a3c94e134026a84fb
Naelle Zephir <nzephir@bu.edu>
2019-03-23 21:22:36 -0400

    updated imports

--- foodshare/food/forms.py
+++ foodshare/food/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.db import transaction
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 # , Food
 
