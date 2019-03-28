7d4989af50a850d24609e14d7656760855fab58c
Naelle Zephir <nzephir@bu.edu>
2019-03-28 12:43:36 -0400

    fixed phone field

--- foodshare/food/forms.py
+++ foodshare/food/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
 from .models import User
 # , Food
+
+number_validator = RegexValidator(
+	r'^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$', 
+	"Make sure to use a US phone number"
+)
 
 class SignUpForm(UserCreationForm):
 	name = forms.CharField(
@@ -11,12 +17,10 @@ class SignUpForm(UserCreationForm):
 			}
 		)
 	)
-	phone = forms.IntegerField(
-		widget = forms.NumberInput(
-			attrs = {
-				'placeholder':'Phone Number'
-			}
-		)
+	phone = forms.CharField(
+		required = True,
+		validators = [number_validator],
+		max_length = 10
 	)
 	location = forms.CharField(
 		widget = forms.TextInput(
